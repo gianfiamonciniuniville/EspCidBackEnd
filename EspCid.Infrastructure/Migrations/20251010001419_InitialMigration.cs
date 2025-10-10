3bb638302758d53d626aa34331a9a444bcb8e432
Gian Carlo Fiamoncini <gian.fiamoncini@univille.br>
2025-10-09 21:25:32 -0300

    feat: add first migration

---
+++ EspCid.Infrastructure/Migrations/20251010001419_InitialMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EspCid.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfileImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false, defaultValue: "Autor"),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Bio", "Created", "Email", "PasswordHash", "ProfileImageUrl", "Role", "Updated", "UserName" },
+                values: new object[] { 1, "Test bio user 1", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "user1@user.com", "Teste123", "", "Autor", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "teste" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
--- EspCid.WebApi/Program.cs
+++ EspCid.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using EspCid.Infrastructure;
+using EspCid.WebApi.Middlewares;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContextPool<EspCidDbContext>(b =>
+{
+    var connectionString = builder.Environment.IsDevelopment()
+        ? builder.Configuration.GetConnectionString("DefaultConnection")
+        : builder.Configuration.GetConnectionString("DefaultDockerConnection");
+    b.UseSqlServer(connectionString);
+    b.AddInterceptors(new CommandsInterceptor());
+});
 
 var app = builder.Build();
 
